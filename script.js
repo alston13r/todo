1335e6dd0f04c5f4b8532ccbafb8282efda78ab5
Alston Rodriguez <alston1399@gmail.com>
2025-05-31 07:21:04 -0700

    [refactor] SleekInput
    The SleekInput class is now in its own file

--- script.js
+++ script.js
@@ -81,63 +81,7 @@ function createContextMenu(e, task) {
 
 
 
-class SleekInput {
-  /**
-   * @param {string} placeholder 
-   */
-  constructor(placeholder = '') {
-    this.placeholder = placeholder
 
-    const span = document.createElement('span')
-    span.style.whiteSpace = 'pre'
-    span.style.visibility = 'hidden'
-
-    this.tempSpan = span
-
-    this.builtDom = false
-  }
-
-  /**
-   * @param {string} initialValue
-   * @returns {HTMLElement}
-   */
-  createDom(initialValue = '') {
-    const input = document.createElement('input')
-    input.type = 'text'
-    input.classList.add('sleek')
-    input.placeholder = this.placeholder
-
-    this.domElement = input
-    this.builtDom = true
-
-    input.addEventListener('input', () => {
-      this.updateWidth(input.value)
-    })
-    input.value = initialValue
-    this.updateWidth(initialValue)
-
-    return this.domElement
-  }
-
-  /**
-   * @param {string} input 
-   */
-  updateWidth(input) {
-    if (!this.builtDom) return
-
-    const span = this.tempSpan
-    span.style.font = getComputedStyle(this.domElement).font
-    document.body.appendChild(span)
-
-    if (input.length === 0) span.textContent = this.placeholder
-    else span.textContent = input
-
-    const width = span.offsetWidth
-    this.domElement.style.width = width + 'px'
-
-    span.remove()
-  }
-}
 
 
 
