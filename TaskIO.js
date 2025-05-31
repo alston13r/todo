5e7455388f204d778a862cbdc53e34dc73a4b4fa
Alston Rodriguez <alston1399@gmail.com>
2025-05-31 08:20:57 -0700

    [implement] TaskIO
    Added methods for importing and exporting, will add UI stuff to call them

--- TaskIO.js
+++ TaskIO.js
@@ -16,12 +16,33 @@ class TaskIO {
   static Load() {
     const value = localStorage.getItem(localStorageKeyName)
     if (value !== null && value.length > 0) {
-      const obj = JSON.parse(value)
-      if (obj.root.length === 0) return
+      try {
+        const obj = JSON.parse(value)
+        if (obj.root.length === 0) return
 
-      for (const itemSerial of obj.root) {
-        addTaskToRoot(Task.FromSerial(itemSerial))
+        for (const itemSerial of obj.root) {
+          addTaskToRoot(Task.FromSerial(itemSerial))
+        }
+      } catch (err) {
+        console.error('The key stored in local storage is invalid, please import again', err)
       }
+    }
+  }
+
+  static Export() {
+    const data = localStorage.getItem(localStorageKeyName)
+    navigator.clipboard.writeText(data ?? '')
+      .then(() => console.log('Saved to clipboard'))
+      .catch(err => console.error('Copy failed', err))
+  }
+
+  static async Import() {
+    try {
+      const text = await navigator.clipboard.readText()
+      localStorage.setItem(localStorageKeyName, text)
+      TaskIO.Load()
+    } catch (err) {
+      console.error('Failed to import', err)
     }
   }
 }
