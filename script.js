a9e579167747259bffd448e448becfc8004f8823
Alston Rodriguez <avrodri3@ncsu.edu>
2025-10-27 19:57:15 -0400

    demo with writing back data

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/** @type {Project[]} */
+let data = null
+
 function readInput() {
   /** @type {HTMLTextAreaElement} */
   const inputElement = document.querySelector('#mainInput')
@@ -6,6 +9,19 @@ function readInput() {
   const rawText = inputElement.value
   const projects = TextParser.ParseFullText(rawText)
 
+  data = projects
+
   const descended = projects.map(project => descendProject(project))
   console.log(descended)
+}
+
+function writeData() {
+  if (data === null) throw Error('no data saved')
+
+  /** @type {HTMLTextAreaElement} */
+  const inputElement = document.querySelector('#mainInput')
+  if (!inputElement) throw Error('could not find input element')
+
+  const fullText = data.map(project => descendProject(project))
+  inputElement.value = fullText.join(EOL_SEQUENCE)
 }
